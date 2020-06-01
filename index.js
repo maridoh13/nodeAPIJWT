c3ce67e078e72a04cc6e1cfb8089542de8fccd49
Mariana Dominguez <maridoh@gmail.com>
2020-06-01 12:55:20 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Import routes
+const authRoute = require('./routes/auth');
+
+// Route middleware
+app.use('/api/user', authRoute);
+
+
+
+
+
+app.listen(3000, () => console.log('Server up and running'));
