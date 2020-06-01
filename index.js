0566f1c647eb428959aec765fb4e726a506ef827
Mariana Dominguez <maridoh@gmail.com>
2020-06-01 13:32:47 -0700

    created User model and connected to DB

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // Import routes
 const authRoute = require('./routes/auth');
 
+
+dotenv.config();
+
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to DB')
+);
+
+//Middleware
+app.use(express.json());
 // Route middleware
 app.use('/api/user', authRoute);
 
