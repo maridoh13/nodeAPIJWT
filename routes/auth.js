c9ef4499ff00b5927e7a78d587ce19ecaeeb87f5
Mariana Dominguez <maridoh@gmail.com>
2020-06-01 14:33:11 -0700

    validation of registration of user working

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,34 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation } = require('../validation')
+
+
 
 router.post('/register', async (req, res) => {
+  //Validation of data before adding user
+  const { error } = registerValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  //Checking if user is already in DB
+
+
+
+  //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
   });
+
   try {
     const savedUser = await user.save();
     res.send(savedUser);
-  } catch(err) {
+  } 
+  catch(err) {
     res.status(400).send(err)
   }
 
 });
-
-
-// router.post('/login', (req, res) => {
-
-// });
 
 
 
